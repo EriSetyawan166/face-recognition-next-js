7cff14fbd3204dfe997f315880a54c9048ff2499
Muhammad Eri Setyawan <erisetyawan34@gmail.com>
2023-07-31 14:02:45 +0700

    fixing for production

--- components/CameraComponent.tsx
+++ components/CameraComponent.tsx
@@ -2,13 +2,13 @@ import React, { useRef, useEffect, useState } from 'react';
 import * as faceapi from 'face-api.js';
 
 const CameraComponent = () => {
-  const videoRef = useRef(null);
-  const canvasRef = useRef(null);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const [videoDims, setVideoDims] = useState({ width: 640, height: 480 });
   const [isDetecting, setIsDetecting] = useState(false);
-  const isDetectingRef = useRef(false); // New ref for tracking detection state
-  const detectionInterval = useRef(null);
-  const noFaceDetectedTimeout = useRef(null);
+  const isDetectingRef = useRef(false);
+  const detectionInterval = useRef<number | null>(null);
+  const noFaceDetectedTimeout = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     const loadModels = async () => {
@@ -20,13 +20,16 @@ const CameraComponent = () => {
         const video = videoRef.current;
         const canvas = canvasRef.current;
         const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
-        video.srcObject = stream;
+        
+        if(video && canvas){
+          video.srcObject = stream;
 
-        video.addEventListener('play', () => {
-          const displaySize = { width: video.width, height: video.height };
-          faceapi.matchDimensions(canvas, displaySize);
-          setVideoDims({ width: video.videoWidth, height: video.videoHeight });
-        });
+          video.addEventListener('play', () => {
+            const displaySize = { width: video.width, height: video.height };
+            faceapi.matchDimensions(canvas, displaySize);
+            setVideoDims({ width: video.videoWidth, height: video.videoHeight });
+          });
+        }
       }
     };
 
@@ -38,52 +41,65 @@ const CameraComponent = () => {
     if (!isDetecting) {
       const video = videoRef.current;
       const canvas = canvasRef.current;
-      const displaySize = { width: video.width, height: video.height };
-
-      detectionInterval.current = setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(
-          video,
-          new faceapi.TinyFaceDetectorOptions()
-        );
-
-        if (detections.length > 1) {
-          alert("Detected more than one face!");
-        }
-
-        if (detections.length === 0) {
-          if (!noFaceDetectedTimeout.current) {
-            noFaceDetectedTimeout.current = setTimeout(() => {
-              alert("No face detected for 5 seconds!");
-              noFaceDetectedTimeout.current = null;
-            }, 5000);
-          }
-        } else {
-          clearTimeout(noFaceDetectedTimeout.current);
-          noFaceDetectedTimeout.current = null;
-        }
-
-        if (isDetectingRef.current) {
-          const resizedDetections = faceapi.resizeResults(
-            detections,
-            displaySize
+      
+      if(video && canvas){
+        const displaySize = { width: video.width, height: video.height };
+  
+        detectionInterval.current = window.setInterval(async () => {
+          const detections = await faceapi.detectAllFaces(
+            video,
+            new faceapi.TinyFaceDetectorOptions()
           );
   
-          canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-          faceapi.draw.drawDetections(canvas, resizedDetections);
-        }
-      }, 100);
-
-      isDetectingRef.current = true;
-      setIsDetecting(true);
+          if (detections.length > 1) {
+            alert("Detected more than one face!");
+          }
+  
+          if (detections.length === 0) {
+            if (!noFaceDetectedTimeout.current) {
+              noFaceDetectedTimeout.current = setTimeout(() => {
+                alert("No face detected for 5 seconds!");
+                noFaceDetectedTimeout.current = null;
+              }, 5000);
+            }
+          } else {
+            if(noFaceDetectedTimeout.current){
+              clearTimeout(noFaceDetectedTimeout.current);
+              noFaceDetectedTimeout.current = null;
+            }
+          }
+  
+          if (isDetectingRef.current) {
+            const resizedDetections = faceapi.resizeResults(
+              detections,
+              displaySize
+            );
+    
+            canvas.getContext('2d')?.clearRect(0, 0, canvas.width, canvas.height);
+            faceapi.draw.drawDetections(canvas, resizedDetections);
+          }
+        }, 100);
+  
+        isDetectingRef.current = true;
+        setIsDetecting(true);
+      }
     }
   };
 
   const stopFaceDetection = () => {
     if (isDetecting) {
-      clearInterval(detectionInterval.current);
-      clearTimeout(noFaceDetectedTimeout.current);
+      if(detectionInterval.current){
+        clearInterval(detectionInterval.current);
+        detectionInterval.current = null;
+      }
+      if(noFaceDetectedTimeout.current){
+        clearTimeout(noFaceDetectedTimeout.current);
+        noFaceDetectedTimeout.current = null;
+      }
       const canvas = canvasRef.current;
-      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+      if(canvas){
+        canvas.getContext('2d')?.clearRect(0, 0, canvas.width, canvas.height);
+      }
       isDetectingRef.current = false;
       setIsDetecting(false);
     }
